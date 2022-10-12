b73b2d53f71f0f2003256ab4e33073d1d1904885
jose3889 <jose3889@hotmail.es>
2022-10-12 09:27:42 -0300

    first

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -21,7 +21,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from COSTUMERS;")
+rows = run_query("SELECT * from COSTUMERS")
 
 # Print results.
 for row in rows:
