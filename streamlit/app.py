8737624a63aae39f929b7415bdc93db10f0c263a
jose3889 <jose3889@hotmail.es>
2022-10-17 10:06:31 -0300

    jose

---
+++ streamlit/app.py
@@ -0,0 +1,40 @@
+# streamlit_app.py
+
+import streamlit as st
+import snowflake.connector
+
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection() # conect
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+run_query("Use database PRUEBA;")
+rows = run_query("SELECT * from COSTUMERS;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
