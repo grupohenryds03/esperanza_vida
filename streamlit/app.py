a328efdd774f4e8c4af6aad05013dcb20765513d
jose3889 <jose3889@hotmail.es>
2022-10-17 10:34:53 -0300

    jose

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,24 @@
 import streamlit as st
 import snowflake.connector
 
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)
+
 # --- HIDE STREAMLIT STYLE ---
 hide_st_style = """
             <style>
