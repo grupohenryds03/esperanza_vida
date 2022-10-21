12a67897574948e7215573cfa472c4dd3dfb9626
Ardilos <lic.poletti@gmail.com>
2022-10-21 12:17:30 -0300

    Pablo

--- streamlit/pages/4_💡_Kpis.py
+++ streamlit/pages/4_💡_Kpis.py
@@ -8,6 +8,7 @@ st.set_page_config(
 
 st.title('Kpis')
 
+lista_Kpi =['Kpi_1','Kpi_2','Kpi_3','Kpi_4']
 '''
 # PROBANDO SI FUNCIONA 1
 '''
@@ -20,6 +21,12 @@ st.title('Kpis')
 ### PROBANDO SI FUNCIONA 3
 '''
 
+eleccion = st.selectbox(
+    'Seleccionar KPI',
+    (lista_Kpi))
+
+
+st.write(eleccion)
 
 st.write('***')
 
@@ -32,3 +39,4 @@ st.write(3, '-  var 3')
 st.write(4, '-  var 4')
 st.write(5, '-  var 5')
 st.write('***')
+
