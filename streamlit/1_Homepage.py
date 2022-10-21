a4443822778e9e5a92bc9ecd19917ace0826c3cf
Jhovanylara <jhovanylara@hotmail.com>
2022-10-20 23:42:36 -0500

    Jhovany2

--- streamlit/1_Homepage.py
+++ streamlit/1_Homepage.py
@@ -134,9 +134,25 @@ with e2:
 st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/diagrama_arq.png')
 st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/5puntos.jpg')
 
-f1,f2,f3=st.columns(3)
+
+st.header("Tecnologías usadas")
+f1,f2,f3,f4=st.columns(4)
+with f1:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/Python_logo_and_wordmark.png', width=100)
 with f2:
-    st.header("Tecnologías usadas")
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/Snowflake_Logo.png', width=100)
+with f3:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/Pandas_logo.png', width=100)
+with f4:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/GitHub_logo.png', width=100)
+g1,g2,g3=st.columns(3)
+with g1:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/Streamlit.png', width=100)
+with g2:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/GitHub_logo.png', width=100)
+with g3:
+    st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/Pandas_logo.png', width=100)
+
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
