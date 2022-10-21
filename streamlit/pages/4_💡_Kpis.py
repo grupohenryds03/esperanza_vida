038683e808de3d18ed87eadf867f80ea7e67961f
Jhovanylara <jhovanylara@hotmail.com>
2022-10-21 13:14:33 -0500

    Jhovany21

--- streamlit/pages/4_💡_Kpis.py
+++ streamlit/pages/4_💡_Kpis.py
@@ -31,7 +31,7 @@ if eleccion=='Mortalidad Infantil':
             disminuya 10% anual en los próximos 5 años para los países de la muestra manteniéndose 
             todo los demás constante.'''
     with a2:
-        st.subheader("Hipotesis")
+        st.subheader("Hipótesis")
         '''Se presenta como Hipótesis que la Esperanza de Vida al nacer en los países Sub-desarrollados,
              aumentara como mínimo un 1% anual y en el caso de los países Desarrollados no llegara
             a aumentar un 1% anual. Esto por efecto de la mejora en la tasa de mortalidad infantil'''
@@ -42,7 +42,7 @@ elif eleccion=='Inversión Publica en Salud':
         '''Se espera que los gobiernos de los países de la muestra aumenten el gasto público en Salud 
             un 10% anual de 5 años manteniéndose todo los demás constante.'''
     with a2:
-        st.subheader("Hipotesis")
+        st.subheader("Hipótesis")
         '''Se presenta como Hipótesis que la variación promedio anual de la Esperanza de Vida al nacer 
             en los países sub-desarrollados de la muestra, aumente como mínimo el doble de la variación 
             promedio anual en los países desarrollados.'''
@@ -54,7 +54,7 @@ elif eleccion=='Mortalidad Materna':
             anual en los próximos 5 años para los países de la muestra manteniéndose todo los demás 
             constante'''
     with a2:
-        st.subheader("Hipotesis")
+        st.subheader("Hipótesis")
         '''Se presenta como Hipótesis que la variación promedio de la esperanza de vida del total de 
             paises de la muestra aumentara como mínimo un 0.5% anual por los próximos 5 años.'''
 elif eleccion=='Ingresos Per Capita':
@@ -64,7 +64,7 @@ elif eleccion=='Ingresos Per Capita':
         '''Se espera que los ingresos per cápita (PBI per cápita (constante U$S 2015)), aumente en 
             promedio un 10% anual por los próximos 5 años en los paises de la muestra.'''
     with a2:
-        st.subheader("Hipotesis")
+        st.subheader("Hipótesis")
         '''Se presenta como Hipótesis que este efecto positivo sobre los ingresos per cápita de la 
             población, generara un efecto positivo en la esperanza de vida, que se presentara como un 
             aumento promedio anual mínimo del 1%.'''
