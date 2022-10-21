1cf887f237bf160aa1374f6d220a1ba9f96ca7ca
Jhovanylara <jhovanylara@hotmail.com>
2022-10-20 19:48:57 -0500

    JHovany8

--- streamlit/1_Homepage.py
+++ streamlit/1_Homepage.py
@@ -12,8 +12,9 @@ st.set_page_config(
     page_icon="👋",
 )
 
-with open('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/streamlit/style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+f='https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/streamlit/style.css'
+
+st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 col1,col2,col3=st.columns(3)
 with col2:
