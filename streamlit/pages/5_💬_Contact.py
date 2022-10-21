6bee65b6a53d92ce07c74b68abd5b9b84da3bac8
Jhovanylara <jhovanylara@hotmail.com>
2022-10-21 11:30:10 -0500

    JHovany11

--- streamlit/pages/5_💬_Contact.py
+++ streamlit/pages/5_💬_Contact.py
@@ -7,7 +7,23 @@ st.set_page_config(
 
 st.title("Contact:")
 
-st.subheader("José Toledo")
-st.subheader("Pablo Poletti")
-st.subheader("Rodrigo Álvarez Ruiz")
-st.subheader("Jhovany Lara")
+a1,a2=st.columns(2)
+with a1:
+    st.subheader("José Toledo")
+with a2:
+    'toledojm@outlook.com'
+b1,b2=st.columns(2)
+with b1:
+    st.subheader("Pablo Poletti")
+with b2:
+    'lic.poletti@gmail.com'
+c1,c2=st.columns(2)
+with c1:
+    st.subheader("Rodrigo Álvarez Ruiz")
+with c2:
+    'Rodrigorear94@gmail.com'
+d1,d2=st.columns(2)
+with d1:
+    st.subheader("Jhovany Lara")
+with d2:
+    'jhovanylara@hotmail.com'
