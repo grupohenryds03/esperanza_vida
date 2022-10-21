5e3de71df7930235df927c756d32b67b8698d003
Ardilos <lic.poletti@gmail.com>
2022-10-21 10:59:50 -0300

    Pablo

---
+++ streamlit/pages/4_Kpis.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Kpis')
+st.write('***')
+
+
+st.write('Se seleccionaron 4 Kpis.')
+st.write('Utilizando las siguientes variables:')
+st.write(1, '-  var 1')
+st.write(2, '-  var 2')
+st.write(3, '-  var 3')
+st.write(4, '-  var 4')
+st.write(5, '-  var 5')
+st.write('***')
