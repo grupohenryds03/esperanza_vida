b2965b34e430418f10fc41bcc269acba1db81cda
rodrigoalvarezruiz <74667425+rodrigoalvarezruiz@users.noreply.github.com>
2022-10-24 18:25:20 -0300

    commit rodri

--- streamlit/pages/2_📊_variables.py
+++ streamlit/pages/2_📊_variables.py
@@ -54,7 +54,7 @@ st.write('***')
 df = pd.read_csv('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/datasets/EV.csv')
 st.write(df.head())
 trace  = go.Bar(
-                x=df['ID_PAIS'].tolist(),
+                x=df.columns.tolist(),
                 y=df['VALOR'].tolist(),
                 showlegend = False
                                     )
