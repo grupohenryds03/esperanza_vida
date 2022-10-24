7d7ed7eca118c1f3909bbcc38f86f3f642199812
rodrigoalvarezruiz <74667425+rodrigoalvarezruiz@users.noreply.github.com>
2022-10-24 18:09:16 -0300

    commit rodri

--- streamlit/pages/2_📊_variables.py
+++ streamlit/pages/2_📊_variables.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import missingno as msno
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.set_page_config(
     page_title="Multipage App",
