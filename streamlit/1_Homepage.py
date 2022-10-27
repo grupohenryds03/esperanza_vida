ec4bcad241ea8cc2d4e027120423f6734958076b
Jhovanylara <jhovanylara@hotmail.com>
2022-10-27 14:01:56 -0500

    Jhovany70

--- streamlit/1_Homepage.py
+++ streamlit/1_Homepage.py
@@ -13,20 +13,12 @@ st.set_page_config(
     page_icon="👋",
 )
 
-# Open page style
-#with open('style.css') as f:
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data=f.read()
-    return base64.b64encode(data).decode()
-img= get_img_as_base64("https://github.com/grupohenryds03/esperanza_vida/blob/main/imagenes/houses-background.jpg?raw=true")
 
 page_style = """
             <style>
             [data-testid="stAppViewContainer"] {
             
-            background-image: url("data:image/jpg;base64,{img}");
+            background-image: url("https://github.com/grupohenryds03/esperanza_vida/blob/main/imagenes/houses-background.jpg?raw=true");
             background-size: cover;
             background-position: right;
             }
