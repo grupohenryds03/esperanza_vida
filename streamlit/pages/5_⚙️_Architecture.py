3754f459a3cc25f7ac09950abf455952d62f57a9
jose3889 <jose3889@hotmail.es>
2022-10-26 23:57:53 -0300

    commit jose

--- streamlit/pages/5_⚙️_Architecture.py
+++ streamlit/pages/5_⚙️_Architecture.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector
 
+
 st.set_page_config(
     page_title='Architecture',
     page_icon='⚙️',
@@ -29,6 +30,18 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.image('https://raw.githubusercontent.com/grupohenryds03/esperanza_vida/main/imagenes/diagrama_solo.jpg')
 
 
+'''
+- La arquitectura sigue cinco pasos principales: el primero para analizar las fuentes de datos, el segundo para la Extracción, Trasformación (limpieza) y Carga (Load) llamado por sus siglas ETL. El tercer paso donde se realiza la carga incremental a la base de datos relacional, el cuarto la carga incremental y el último paso donde se realizan las consultas necesarias para ser utilizada en modelos de ML y visualización en dashboard.
+- El entorno de trabajo para el ETL se desarrola en AIRFLOW dentro de una cloud maching de HEROKU. Acceso a la api: https://etl-latin-data.herokuapp.com/
+- Para el armado del datalake se ingestan los datos en el entorno STAGE de SNOWFLAKE en formato .csv comprimido en .gz (pueden ser tambien json, parquet, xlsx).
+- En el caso de la base de datos relacional se utiliza SNOWFLAKE con la creación de un warehouse para su mantenimiento e ingesta incremental.
+- para el modelado en ML y visualización de datos se realiza querys según los requerimientos del cliente.
+'''
+
+
+
+
+
 @st.experimental_singleton
 def init_connection():
     return snowflake.connector.connect(
@@ -49,4 +62,45 @@ def run_query(query):
         cur.execute(query)
         return cur.fetch_pandas_all()
 
+from info import *
+import plotly.graph_objects as go
 
+col1,col2=st.columns(2)
+
+with col1:
+    option_pais = st.selectbox(
+        'Elegir el país de la lista despleglable',
+        dic_pais2.keys) #lista_codigo_pais
+
+    eleccion_pais=dic_pais2[option_pais]
+    id_pais=dic_id_pais[eleccion_pais] #option
+
+    'La selección fue:', eleccion_pais #dic_pais2[option]
+
+with col2:
+    option_var = st.selectbox(
+        'Elegir el país de la lista despleglable',
+        dic_indicador2.keys) #lista_codigo_pais
+
+    eleccion_var=dic_indicador2[option_var]
+    id_var=dic_indicador[eleccion_var] #option
+
+sql_var =f"SELECT ANIO, VALOR FROM EV WHERE ID_INDICADOR=31 AND ANIO<=2020 AND ID_PAIS='{id_pais}'"
+df_esp=run_query(sql_var)
+
+sql_pais =f"SELECT ANIO, VALOR FROM EV WHERE ID_INDICADOR={id_var} AND ANIO<=2020 AND ID_PAIS='{id_pais}'"
+df_var=run_query(sql_pais)
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df_esp.ANIO, 
+                    y=df_esp.VALOR,
+                    mode='lines',
+                    marker_color='#FF0000',
+                    line=dict(width=0.8)))
+
+fig.add_trace(go.Scatter(x=df_var.ANIO, 
+                    y=df_var.VALOR,#option
+                    mode='lines',
+                    marker_color='#00FF00',
+                    name='Relación Esperanza de Vida',
+                    line=dict(width=0.8)))
