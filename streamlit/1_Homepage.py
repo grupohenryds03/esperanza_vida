d7959f0a8d011249a85c42d806f96f08254d49d9
Jhovanylara <jhovanylara@hotmail.com>
2022-10-27 16:44:34 -0500

    Jhovany88

--- streamlit/1_Homepage.py
+++ streamlit/1_Homepage.py
@@ -23,7 +23,7 @@ page_style = """
             background-position: right;
             }
             [data-testid="stSidebar"]{
-            background-image: url("https://github.com/grupohenryds03/esperanza_vida/blob/main/imagenes/WallpaperRocky.jpg?raw=true");
+            background-image: url("https://github.com/grupohenryds03/esperanza_vida/blob/main/imagenes/imageLE.png?raw=true");
             background-size: cover;
             background-position: right;
             }
