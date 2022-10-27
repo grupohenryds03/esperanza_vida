d34bc94a431d86ca41f960ac98be1dd41059db09
jose3889 <jose3889@hotmail.es>
2022-10-27 00:23:08 -0300

    commit jose

--- streamlit/pages/5_⚙️_Architecture.py
+++ streamlit/pages/5_⚙️_Architecture.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
+from info import *
+import plotly.graph_objects as go
 
 
 st.set_page_config(
@@ -62,15 +64,14 @@ def run_query(query):
         cur.execute(query)
         return cur.fetch_pandas_all()
 
-from info import *
-import plotly.graph_objects as go
+
 
 col1,col2=st.columns(2)
 
 with col1:
     option_pais = st.selectbox(
         'Elegir el país de la lista despleglable',
-        dic_pais2.keys) #lista_codigo_pais
+        lista_codigo_pais) #lista_codigo_pais
 
     eleccion_pais=dic_pais2[option_pais]
     id_pais=dic_id_pais[eleccion_pais] #option
@@ -80,7 +81,7 @@ with col1:
 with col2:
     option_var = st.selectbox(
         'Elegir el país de la lista despleglable',
-        dic_indicador2.keys) #lista_codigo_pais
+        Indicador) #lista_codigo_pais
 
     eleccion_var=dic_indicador2[option_var]
     id_var=dic_indicador[eleccion_var] #option
@@ -91,3 +92,16 @@ df_esp=run_query(sql_var)
 sql_pais =f"SELECT ANIO, VALOR FROM EV WHERE ID_INDICADOR={id_var} AND ANIO<=2020 AND ID_PAIS='{id_pais}'"
 df_var=run_query(sql_pais)
 
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df_esp.ANIO, 
+                    y=df_esp.VALOR,
+                    mode='lines',
+                    marker_color='#FF0000',
+                    line=dict(width=0.8)))
+
+fig.add_trace(go.Scatter(x=df_var.ANIO, 
+                    y=df_var.VALOR,#option
+                    mode='lines',
+                    marker_color='#00FF00',
+                    name='Relación Esperanza de Vida',
+                    line=dict(width=0.8)))
