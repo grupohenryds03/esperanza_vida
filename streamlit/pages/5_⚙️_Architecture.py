ea241b65cb8ee7ebb35d397bbd29dfb4fe7197bb
jose3889 <jose3889@hotmail.es>
2022-10-27 00:20:07 -0300

    commit jose

--- streamlit/pages/5_⚙️_Architecture.py
+++ streamlit/pages/5_⚙️_Architecture.py
@@ -91,16 +91,3 @@ df_esp=run_query(sql_var)
 sql_pais =f"SELECT ANIO, VALOR FROM EV WHERE ID_INDICADOR={id_var} AND ANIO<=2020 AND ID_PAIS='{id_pais}'"
 df_var=run_query(sql_pais)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df_esp.ANIO, 
-                    y=df_esp.VALOR,
-                    mode='lines',
-                    marker_color='#FF0000',
-                    line=dict(width=0.8)))
-
-fig.add_trace(go.Scatter(x=df_var.ANIO, 
-                    y=df_var.VALOR,#option
-                    mode='lines',
-                    marker_color='#00FF00',
-                    name='Relación Esperanza de Vida',
-                    line=dict(width=0.8)))
