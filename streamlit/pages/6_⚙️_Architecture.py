21f48d1e8194dce851a394d0594234208e18fea2
jose3889 <jose3889@hotmail.es>
2022-10-28 10:34:32 -0300

    commit jose

--- streamlit/pages/6_⚙️_Architecture.py
+++ streamlit/pages/6_⚙️_Architecture.py
@@ -17,9 +17,9 @@ page_style = """
             """
 st.markdown(page_style, unsafe_allow_html=True)
 
-'# Project architecture'
+'#Architecture'
 
-'_The proyect architecture follows five main steps: the first one is to study and analyze the data source. The second one extract particular data from the source. The third one is data transformation and cleaning. The fourth one is incremental load of data into the relational tables. Finally, the last step implements queries to extract data for machine learning (ML) algorithms and visualizes it by charts in the dashboard._'
+'_The architecture project follows five main steps: the first one is to study and analyze the data source. The second one extract particular data from the source. The third one is data transformation and cleaning. The fourth one is incremental load of data into the relational tables. Finally, the last step implements queries to extract data for machine learning (ML) algorithms and visualizes it by charts in the dashboard._'
 
 st.write('***')
 '### Detail description'
