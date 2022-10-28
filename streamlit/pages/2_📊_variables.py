4fc5eb3d69885569bb98f0853aa78447555c94ae
Jhovanylara <jhovanylara@hotmail.com>
2022-10-27 19:10:33 -0500

    Jhovany106

--- streamlit/pages/2_📊_variables.py
+++ streamlit/pages/2_📊_variables.py
@@ -26,7 +26,15 @@ page_style = """
             }
             </style>
             """
-#background-Color: blue;
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
             
 st.markdown(page_style, unsafe_allow_html=True)
 st.title('Selected Variables')
