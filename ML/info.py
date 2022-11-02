39109a175074f090538a12e62dedee1dd4ab3bf3
jose3889 <jose3889@hotmail.es>
2022-11-01 23:37:00 -0300

    commit jose

---
+++ ML/info.py
@@ -0,0 +1,94 @@
+dic_imcom={0: 'Alto ingreso', 1: 'Ingreso medio alto', 2: 'Ingreso medio bajo'}
+
+dic_indicador={0: 'AG.LND.PRCP.MM', 1: 'EN.ATM.CO2E.KT', 2: 'SP.POP.GROW', 3: 'SE.PRM.CUAT.FE.ZS', 4: 'SE.TER.ENRR', 5: 'SE.ADT.LITR.ZS', 6: 'CSV from WHO', 7: 'SH.IMM.IDPT', 8: 'SH.IMM.MEAS', 9: 'SH.HIV.INCD.TL.P3', 10: 'SH.DYN.MORT', 11: 'SH.DTH.0509', 12: 'SH.DTH.IMRT.IN', 13: 'CSV from WHO', 14: 'SH.PRV.SMOK.MA', 15: 'SH.PRV.SMOK.FE', 16: 'SH.STA.MMRT', 17: 'SH.DYN.MORT', 18: 'SP.DYN.AMRT.MA', 19: 'SP.DYN.AMRT.FE', 20: 'SH.ALC.PCAP.LI', 21: 'EG.CFT.ACCS.RU.ZS', 22: 'SE.XPD.CPRM.ZS', 23: 'SH.XPD.CHEX.GD.ZS', 24: 'SL.UEM.TOTL.NE.ZS', 25: 'SP.DYN.LE00.MA.IN', 26: 'SP.DYN.LE00.FE.IN', 27: 'SP.DYN.LE00.IN', 28: 'GC.XPN.TOTL.GD.ZS', 29: 'SE.XPD.TOTL.GD.ZS', 30: 'NY.GDP.PCAP.KD', 31: 'EN.POP.SLUM.UR.ZS', 32: 'SP.RUR.TOTL.ZS', 33: 'SP.URB.TOTL.IN.ZS', 34: 'GB.XPD.RSDV.GD.ZS', 35: 'SP.POP.SCIE.RD.P6', 36: 'SI.POV.MDIM', 37: 'BG.GSR.NFSV.GD.ZS'}
+
+dic_pais={'United States': 'USA', 'Canada': 'CAN', 'Mexico': 'MEX', 'Costa Rica': 'CRI', 'Panama': 'PAN', 'Brazil': 'BRA', 'Argentina': 'ARG', 'Chile': 'CHL', 'Uruguay': 'URY', 'Bolivia': 'BOL', 'Peru': 'PER', 'Egypt, Arab Rep.': 'EGY', 'Libya': 'LBY', 'South Africa': 'ZAF', 'Nigeria': 'NGA', 'Morocco': 'MAR', 'Australia': 'AUS', 'China': 'CHN', 'India': 'IND', 'Thailand': 'THA', 'Japan': 'JPN', 'Korea Rep': 'KOR', 'Israel': 'ISR', 'Saudi Arabia': 'SAU', 'Malaysia': 'MYS', 'Indonesia': 'IDN', 'Russian Federation': 'RUS', 'Turkiye': 'TUR', 'Spain': 'ESP', 'Bulgaria': 'BGR', 'France': 'FRA', 'Italy': 'ITA', 'Germany': 'DEU', 'United Kingdom': 'GBR', 'Norway': 'NOR', 'Sweden': 'SWE', 'Greece': 'GRC'}
+dic_pais2={'USA': 'United States', 'CAN': 'Canada', 'MEX': 'Mexico', 'CRI': 'Costa Rica', 'PAN': 'Panama', 'BRA': 'Brazil', 'ARG': 'Argentina', 'CHL': 'Chile', 'URY': 'Uruguay', 'BOL': 'Bolivia', 'PER': 'Peru', 'EGY': 'Egypt, Arab Rep.', 'LBY': 'Libya', 'ZAF': 'South Africa', 'NGA': 'Nigeria', 'MAR': 'Morocco', 'AUS': 'Australia', 'CHN': 'China', 'IND': 'India', 'THA': 'Thailand', 'JPN': 'Japan', 'KOR': 'Korea, Rep.', 'ISR': 'Israel', 'SAU': 'Saudi Arabia', 'MYS': 'Malaysia', 'IDN': 'Indonesia', 'RUS': 'Russian Federation', 'TUR': 'Turkiye', 'ESP': 'Spain', 'BGR': 'Bulgaria', 'FRA': 'France', 'ITA': 'Italy', 'DEU': 'Germany', 'GBR': 'United Kingdom', 'NOR': 'Norway', 'SWE': 'Sweden', 'GRC': 'Greece'}
+
+dic_id_pais={'ARG': 0, 'AUS': 1, 'BGR': 2, 'BOL': 3, 'BRA': 4, 'CAN': 5, 'CHL': 6, 'CHN': 7, 'CRI': 8, 'DEU': 9, 'EGY': 10, 'ESP': 11, 'FRA': 12, 'GBR': 13, 'GRC': 14, 'IDN': 15, 'IND': 16, 'ISR': 17, 'ITA': 18, 'JPN': 19, 'KOR': 20, 'LBY': 21, 'MAR': 22, 'MEX': 23, 'MYS': 24, 'NGA': 25, 'NOR': 26, 'PAN': 27, 'PER': 28, 'RUS': 29, 'SAU': 30, 'SWE': 31, 'THA': 32, 'TUR': 33, 'URY': 34, 'USA': 35, 'ZAF': 36}
+
+
+pais = ['Mexico', 'Canada','United States','Costa Rica','Panama','Brazil','Argentina','Chile','Uruguay','Bolivia','Peru','Egypt, Arab Rep.','Libya','South Africa','Nigeria','Morocco','Australia','China','India','Thailand','Japan','Korea, Rep.','Israel','Saudi Arabia','Malaysia','Indonesia','Russian Federation','Turkiye','Spain','Bulgaria','France','Italy','Germany','United Kingdom','Norway','Sweden','Greece']
+lista_codigo_pais= ['MEX','CRI','PAN','BRA','ARG','CHL','URY','BOL','USA','CAN','PER','EGY','LBY','ZAF','NGA','MAR','AUS','CHN','IND','THA','JPN','KOR','ISR','SAU','MYS','IDN','RUS','TUR','ESP','BGR','FRA','ITA','DEU','GBR','NOR','SWE','GRC']
+
+
+dic_indicador2={'Average precipitation in depth (mm per year)': 'AG.LND.PRCP.MM', 'emisiones de CO2 (kt)': 'AG.LND.PRCP.MM', 'crecimiento de la poblacion (porcentaje anual)': 'AG.LND.PRCP.MM', 'Educational attainment, at least completed primary, population 25+ years, female (%) (cumulative)': 'AG.LND.PRCP.MM', 'School enrollment, tertiary (porcentaje gross)': 'AG.LND.PRCP.MM', 'Tasa de alfabetizacion, total adultos (porcentaje de personas)': 'AG.LND.PRCP.MM', 'Hepatitis B (HepB3) immunization coverage among 1-year-olds (%)': 'AG.LND.PRCP.MM', 'Immunization, DPT (porcentaje of children ages 12-23 months)': 'AG.LND.PRCP.MM', 'Immunization, measles (porcentaje of children ages 12-23 months)': 'AG.LND.PRCP.MM', 'Incidence of HIV, all (per 1,000 uninfected population)': 'AG.LND.PRCP.MM', 'Mortality rate, under-5 (per 1,000 live births)': 'AG.LND.PRCP.MM', 'Number of deaths ages 5-9 years': 'AG.LND.PRCP.MM', 'Number of infant deaths (per 1,000 live births)': 'AG.LND.PRCP.MM', 'Polio (Pol3) immunization covergae among 1-year-olds (%)': 'AG.LND.PRCP.MM', 'prevalencia del consumo de tabaco, hombres': 'AG.LND.PRCP.MM', 'prevalencia del consumo de tabaco, mujeres': 'AG.LND.PRCP.MM', 'tasa de mortalidad materna (cada 100.000 nacidos vivos)': 'AG.LND.PRCP.MM', 'tasa de mortalidad menores de 5 años (por 1000nacidos vivos)': 'AG.LND.PRCP.MM', 'tasa de mortalidad, adultos hombres (por cada 1000 adultos)': 'AG.LND.PRCP.MM', 'tasa de mortalidad, adultos mujeres (por cada 1000 adultos)': 'AG.LND.PRCP.MM', 'Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)': 'AG.LND.PRCP.MM', 'Access to clean fuels and technologies for cooking, rural (porcentaje of rural population)': 'AG.LND.PRCP.MM', 'Current education expenditure, primary (porcentaje of total expenditure in primary public institutions)': 'AG.LND.PRCP.MM', 'Current health expenditure (porcentaje of GDP)': 'AG.LND.PRCP.MM', 'desempleo total (porcentaje de la poblacion laboral)': 'AG.LND.PRCP.MM', 'esperanza de vida al nacer, hombres (años)': 'AG.LND.PRCP.MM', 'esperanza de vida al nacer, muejres (años)': 'AG.LND.PRCP.MM', 'esperanza de vida al nacer, Total (años)': 'AG.LND.PRCP.MM', 'gasto publico (porcentaje del pib)': 'AG.LND.PRCP.MM', 'gasto publico en educación, total (porcentaje del pbi)': 'AG.LND.PRCP.MM', 'GDP per capita (constant 2015 US$)': 'AG.LND.PRCP.MM', 'poblacion que vive en barrios marginales (% de la poblacion urbana)': 'AG.LND.PRCP.MM', 'Población Rural (porcentaje de la poblacion total)': 'AG.LND.PRCP.MM', 'poblacion urbana (%poblacion total)': 'AG.LND.PRCP.MM', 'Research and development expenditure (porcentaje of GDP)': 'AG.LND.PRCP.MM', 'Researchers in R&D (per million people)': 'AG.LND.PRCP.MM', 'tasa de recuento de la pobreza, multidimensional (porcentaje de la poblacion total)': 'AG.LND.PRCP.MM', 'Trade in services (porcentaje of GDP)': 'AG.LND.PRCP.MM'}
+
+Indicador = ['Average precipitation in depth (mm per year)',
+'emisiones de CO2 (kt)',
+'crecimiento de la poblacion (porcentaje anual)',
+'Educational attainment, at least completed primary, population 25+ years, female (%) (cumulative)',
+'School enrollment, tertiary (porcentaje gross)',
+'Tasa de alfabetizacion, total adultos (porcentaje de personas)',
+'Hepatitis B (HepB3) immunization coverage among 1-year-olds (%)',
+'Immunization, DPT (porcentaje of children ages 12-23 months)',
+'Immunization, measles (porcentaje of children ages 12-23 months)',
+'Incidence of HIV, all (per 1,000 uninfected population)',
+'Mortality rate, under-5 (per 1,000 live births)',
+'Number of deaths ages 5-9 years',
+'Number of infant deaths (per 1,000 live births)',
+'Polio (Pol3) immunization covergae among 1-year-olds (%)',
+'prevalencia del consumo de tabaco, hombres',
+'prevalencia del consumo de tabaco, mujeres',
+'tasa de mortalidad materna (cada 100.000 nacidos vivos)',
+'tasa de mortalidad menores de 5 años (por 1000nacidos vivos)',
+'tasa de mortalidad, adultos hombres (por cada 1000 adultos)',
+'tasa de mortalidad, adultos mujeres (por cada 1000 adultos)',
+'Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)',
+'Access to clean fuels and technologies for cooking, rural (porcentaje of rural population)',
+'Current education expenditure, primary (porcentaje of total expenditure in primary public institutions)',
+'Current health expenditure (porcentaje of GDP)',
+'desempleo total (porcentaje de la poblacion laboral)',
+'esperanza de vida al nacer, hombres (años)',
+'esperanza de vida al nacer, muejres (años)',
+'esperanza de vida al nacer, Total (años)',
+'gasto publico (porcentaje del pib)',
+'gasto publico en educación, total (porcentaje del pbi)',
+'GDP per capita (constant 2015 US$)',
+'poblacion que vive en barrios marginales (porcentaje de la poblacion urbana)',
+'Población Rural (porcentaje de la poblacion total)',
+'poblacion urbana (%poblacion total)',
+'Research and development expenditure (porcentaje of GDP)',
+'Researchers in R&D (per million people)',
+'tasa de recuento de la pobreza, multidimensional (porcentaje de la poblacion total)',
+'Trade in services (porcentaje of GDP)'
+]
+code_indicador = ['AG.LND.PRCP.MM',
+'EN.ATM.CO2E.KT',
+'SP.POP.GROW',
+'SE.PRM.CUAT.FE.ZS',
+'SE.TER.ENRR',
+'SE.ADT.LITR.ZS',
+'CSV from WHO',
+'SH.IMM.IDPT',
+'SH.IMM.MEAS',
+'SH.HIV.INCD.TL.P3',
+'SH.DYN.MORT',
+'SH.DTH.0509',
+'SH.DTH.IMRT.IN',
+'CSV from WHO',
+'SH.PRV.SMOK.MA',
+'SH.PRV.SMOK.FE',
+'SH.STA.MMRT',
+'SH.DYN.MORT',
+'SP.DYN.AMRT.MA',
+'SP.DYN.AMRT.FE',
+'SH.ALC.PCAP.LI',
+'EG.CFT.ACCS.RU.ZS',
+'SE.XPD.CPRM.ZS',
+'SH.XPD.CHEX.GD.ZS',
+'SL.UEM.TOTL.NE.ZS',
+'SP.DYN.LE00.MA.IN',
+'SP.DYN.LE00.FE.IN',
+'SP.DYN.LE00.IN',
+'GC.XPN.TOTL.GD.ZS',
+'SE.XPD.TOTL.GD.ZS',
+'NY.GDP.PCAP.KD',
+'EN.POP.SLUM.UR.ZS',
+'SP.RUR.TOTL.ZS',
+'SP.URB.TOTL.IN.ZS',
+'GB.XPD.RSDV.GD.ZS',
+'SP.POP.SCIE.RD.P6',
+'SI.POV.MDIM',
+'BG.GSR.NFSV.GD.ZS'
+]
